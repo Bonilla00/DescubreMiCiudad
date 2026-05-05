e8fd56eb01caa43d88ea7bc507fb36a6956d43ff
Jainer <jainer.bonilla00@usc.edu.co>
2026-05-05 14:22:13 -0500

    fix: add missing foundation.dart import for debugPrint

--- lib/services/lugares_service.dart
+++ lib/services/lugares_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:async';
+import 'package:flutter/foundation.dart'; // 🔥 IMPORTAR PARA debugPrint
 import 'package:http/http.dart' as http;
 import '../constants/api.dart';
 import '../models/place_model.dart';
